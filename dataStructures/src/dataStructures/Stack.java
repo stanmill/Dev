080d388df087b17d32e7cdbf644a3d6fb13bf84a
stanmillz <stanley.pena@niocoders.com>
2019-08-15 22:32:54 -0400

    stack

---
+++ dataStructures/src/dataStructures/Stack.java
@@ -0,0 +1,38 @@
+package dataStructures;
+
+import java.util.Vector;
+
+public class Stack<E> {
+	
+	Vector<E> list = new Vector<E>();
+	E element;
+	
+	
+	void push(E element) {
+		
+		list.add(element);	
+	}
+	
+	E pop() {
+		
+		E lastElement = list.lastElement();
+		list.remove(lastElement);
+		
+		return lastElement;		
+	}
+	
+	int checkSize() {
+		return list.size();
+	}
+	
+	boolean isEmpty() {
+		return list.isEmpty();
+	}
+	
+	E peek() {
+		
+		//if(list.isEmpty()) System.e();
+		return list.lastElement();
+	}
+
+}
