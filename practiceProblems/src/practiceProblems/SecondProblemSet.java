221155a5f6f85a2afe1f8ea2c9dd5fe552d9da8d
stanmill <stanley.pena29@gmail.com>
2019-09-01 20:05:31 -0400

    summation problem

--- practiceProblems/src/practiceProblems/SecondProblemSet.java
+++ practiceProblems/src/practiceProblems/SecondProblemSet.java
@@ -6,7 +6,8 @@ public class SecondProblemSet {
 		
 		String s = "abbrhcccc";
 		System.out.println(compress(s));
-		System.out.println(theNextPerfectSquare(30));
+		System.out.println(theNextPerfectSquare(50));
+		System.out.println(summation(5));
 	}
 	
 	
@@ -19,14 +20,15 @@ public class SecondProblemSet {
 		
 		for (int i = 0; i < s.length(); i++) {
 			count++;		
-			
+			// compares the two indexes to see if they are the same
 			if(i +1 >= s.length() || s.charAt(i) != s.charAt(i + 1)) {
+				// if they are the same it adds it to the string builder and resets the count
 				 str.append(s.charAt(i));
 				 str.append(count);
 					count = 0;
 			} 
 		}
-			
+	
 		return new String (str);
 	}
 	
@@ -40,5 +42,18 @@ public class SecondProblemSet {
 		// reverts the square root back to a perfect square
 		return sqr * sqr;
 	}
+	
+	/* Write a function that returns the sum of numbers up to n */
+	
+	static int summation(int n) {
+        
+		int sum = 0;
+		
+        for (int i = 0; i <= n; i++) {
+        	sum = sum +i;
+        
+        }
+        return sum;
+    }
 
 }
