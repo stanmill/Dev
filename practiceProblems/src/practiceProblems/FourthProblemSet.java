ee859c0a5141c615e965c71335875d7a6af98751
stanmill <stanley.pena29@gmail.com>
2019-09-17 23:25:29 -0400

    update

--- practiceProblems/src/practiceProblems/FourthProblemSet.java
+++ practiceProblems/src/practiceProblems/FourthProblemSet.java
@@ -5,7 +5,9 @@ public class FourthProblemSet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n = 10;
-		System.out.println(fibb(n));;
+		int[] arr = {-1000,1,4,50,-100, 200, 1000};
+		System.out.println(fibb(n));
+		System.out.println(findTheLongest(arr));
 
 	}
 	
@@ -18,6 +20,32 @@ public class FourthProblemSet {
 		else if(n == 1) return 1;
 		
 		return fibb(n-1)+fibb(n-2);
+	}
+	
+	/* find the number with the longest digits*/
+	
+	static int findTheLongest(int[] numbers) {
+		
+		int max =0;
+		int count = 0;
+		int currentNum = 0;
+		int temp =0;
+		
+		for(int i=0; i< numbers.length; i++) {
+			count =0;
+			temp = numbers[i];
+			while(temp != 0) {	
+				temp /= 10;
+				count++;
+			}
+			
+			if(count > max) {
+				currentNum = numbers[i];
+				max = count;
+			}	
+		}	
+		
+		return currentNum;
 	}
 
 }
