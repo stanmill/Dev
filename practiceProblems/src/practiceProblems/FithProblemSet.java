4d83079eee4c92174eb127fb78b4c1becf1bdcb3
stanmill <stanley.pena29@gmail.com>
2019-09-22 16:22:04 -0400

    new porblem set

---
+++ practiceProblems/src/practiceProblems/FithProblemSet.java
@@ -0,0 +1,25 @@
+package practiceProblems;
+
+import java.util.Arrays;
+
+public class FithProblemSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/* Compare two string if they have the same characters*/
+	
+	public static boolean isAnagram(String test, String original) {
+		
+		char[] origin = original.toLowerCase().toCharArray();
+		char[] compare = test.toLowerCase().toCharArray();
+		Arrays.sort(origin);
+		Arrays.sort(compare);
+		
+		if(Arrays.equals(origin, compare)) return true;
+		
+	    return false;
+	  }
+
+}
